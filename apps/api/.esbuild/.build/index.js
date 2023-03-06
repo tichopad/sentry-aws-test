c796532b30c4b1723b36fbab9efbb8aa22a26ed7
Michael Tichopad <michael@tichopad.dev>
2023-03-06 20:27:19 +0100

    Add ts-reset, tsconfig

--- apps/api/.esbuild/.build/index.js
+++
@@ -1,1 +0,0 @@
-module.exports.handler=async e=>({statusCode:200,body:JSON.stringify({message:"Go Serverless v3.0! Your function executed successfully!",input:e},null,2)});
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,47 @@
 import type { APIGatewayEvent } from 'aws-lambda'
+import SchemaBuilder from '@pothos/core'
+import { graphql } from 'graphql'
+import { z } from 'zod'
 
-module.exports.handler = async (event: APIGatewayEvent) => {
+const eventBodySchema = z.object({
+  query: z.string(),
+  variables: z.record(z.any()).optional(),
+})
+
+const builder = new SchemaBuilder({})
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      resolve: () => 'world',
+    }),
+    bye: t.string({
+      args: {
+        count: t.arg({
+          type: 'Int',
+          required: true,
+        }),
+      },
+      resolve: (_, args) => {
+        return new Array(args.count)
+          .fill(null)
+          .map(() => 'Bye')
+          .join('!')
+      },
+    }),
+  }),
+})
+const schema = builder.toSchema()
+
+export const handler = async (event: APIGatewayEvent) => {
+  const rawEventBody = JSON.parse(event.body ?? '')
+  const eventBody = eventBodySchema.parse(rawEventBody)
+  const result = await graphql({
+    schema,
+    source: eventBody.query,
+    variableValues: eventBody.variables,
+  })
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify(result),
   }
 }
