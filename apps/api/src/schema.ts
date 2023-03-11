612351ce24550f68cc085a1e8098374b596fcafa
Michael Tichopad <michael@tichopad.dev>
2023-03-11 23:24:23 +0100

    Try Pothos + Sentry tracing

--- apps/api/src/schema.ts
+++ apps/api/src/schema.ts
@@ -1,6 +1,19 @@
 import SchemaBuilder from '@pothos/core'
+import TracingPlugin, { isRootField } from '@pothos/plugin-tracing'
+import { createSentryWrapper } from '@pothos/tracing-sentry'
 
-const builder = new SchemaBuilder({})
+const traceResolver = createSentryWrapper({
+  includeArgs: true,
+  includeSource: true,
+})
+
+export const builder = new SchemaBuilder({
+  plugins: [TracingPlugin],
+  tracing: {
+    default: (config) => isRootField(config),
+    wrap: (resolver, options) => traceResolver(resolver, options),
+  },
+})
 
 builder.queryType({
   fields: (t) => ({
