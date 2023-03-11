1e2e3a8ce849213b1a7404a2a4fb56a690dd8543
Michael Tichopad <michael@tichopad.dev>
2023-03-11 23:11:19 +0100

    Correct env linkage for Sentry

--- apps/api/esbuild.plugins.js
+++ apps/api/esbuild.plugins.js
@@ -6,16 +6,18 @@ const { sentryEsbuildPlugin } = require('@sentry/esbuild-plugin')
  * @typedef {import('esbuild').BuildOptions['plugins']} ESBuildPlugins
  * @type {(serverless: Serverless) => ESBuildPlugins}
  */
-module.exports = (serverless) =>
-  process.env.START_SENTRY_RELEASE
-    ? [
-        // Put the Sentry esbuild plugin after all other plugins
-        sentryEsbuildPlugin({
-          org: process.env.SENTRY_ORG,
-          project: process.env.SENTRY_PROJECT,
-          authToken: process.env.SENTRY_AUTH_TOKEN,
-          include: './.esbuild/.build',
-          urlPrefix: '/var/task/',
-        }),
-      ]
-    : []
+module.exports = (serverless) => [
+  // Put the Sentry esbuild plugin after all other plugins
+  sentryEsbuildPlugin({
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    include: './.esbuild/.build',
+    urlPrefix: '/var/task/',
+    dryRun: !process.env.START_SENTRY_RELEASE,
+    silent: !process.env.START_SENTRY_RELEASE,
+    deploy: {
+      env: serverless.service.provider.stage,
+    },
+  }),
+]
